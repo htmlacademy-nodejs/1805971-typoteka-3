b8c8ed1ff5bb86886aea647ebe72ecc60428b975
Anton <61318805+AntonVolkov71@users.noreply.github.com>
2021-07-01 01:44:07 +0300

    module2-task3 goto review (#6)

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,17 @@ const USER_ARGV_INDEX = 2;
 
 const ExitCode = {
   success: 0,
-  invalidArgument: 9,
+  invalidArgument: 9
 };
 
 module.exports = {
   DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode
+};
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };
